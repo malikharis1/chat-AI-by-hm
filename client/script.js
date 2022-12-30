73179bc8a975f847b69dae04f42d74c43119facb
malikharis1 <harismalik1@protonmail.com>
2022-12-30 18:45:04 +0530

    url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codeai-epze.onrender.com', {
+    const response = await fetch('https://codeai-epze.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
